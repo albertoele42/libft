37a4695b2eac448dae4e628f5c444996497ad6d4
Alberto Quintillan Amoros <aquintil@c1r15s6.42madrid.com>
2022-11-04 09:04:21 +0100

    04112022

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aquintil <aquintil@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/22 16:45:28 by aquintil          #+#    #+#              #
-#    Updated: 2022/11/04 07:22:23 by aquintil         ###   ########.fr        #
+#    Updated: 2022/11/04 09:01:29 by aquintil         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -35,7 +35,8 @@ SRCS	=	ft_isalpha.c\
 	  		ft_strdup.c\
 	  		ft_substr.c\
 			ft_strjoin.c\
-			ft_strtrim.c
+			ft_strtrim.c\
+			ft_split.c
 
 OBJS	= $(SRCS:.c=.o)
 NAME	= libft.a
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aquintil <aquintil@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 07:36:20 by aquintil          #+#    #+#             */
-/*   Updated: 2022/11/04 07:23:38 by aquintil         ###   ########.fr       */
+/*   Updated: 2022/11/04 09:02:12 by aquintil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
