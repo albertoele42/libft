3531c06c1a3ed66d9667de511f41a6e439dea46f
Alberto <alberto@tanit.group>
2022-12-14 08:19:12 +0100

    bonus_4

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -14,5 +14,14 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list *last;
 
+	if (*lst)
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+		new->next = NULL;
+	}
+	else
+		*lst = new;
 }
