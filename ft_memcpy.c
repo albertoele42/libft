45a5b22f215a7eeaba36d70a7d364e1acbfdb65a
Alberto Quintillan Amoros <aquintil@c1r6s5.42madrid.com>
2022-12-22 18:33:55 +0100

    francinette_2

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aquintil <aquintil@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 17:30:31 by aquintil          #+#    #+#             */
-/*   Updated: 2022/09/22 17:30:35 by aquintil         ###   ########.fr       */
+/*   Updated: 2022/12/22 18:22:24 by aquintil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		i;
-	char		*s1;
-	const char	*s2;
+	size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
+	if (!dst && !src)
+		return (0);
 	s1 = dst;
-	s2 = src;
+	s2 = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,22 +6,23 @@
 /*   By: aquintil <aquintil@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 17:30:46 by aquintil          #+#    #+#             */
-/*   Updated: 2022/11/01 09:03:54 by aquintil         ###   ########.fr       */
+/*   Updated: 2022/12/22 18:31:48 by aquintil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char		*s1;
 	const char	*s2;
 
-	s1 = dst;
+	s1 = dest;
 	s2 = src;
 	if (s2 >= s1)
-		return (ft_memcpy(s1, s2, len));
-	while (len--)
-		s1[len] = s2[len];
-	return (dst);
+		return (ft_memcpy(s1, s2, n));
+	while (n--)
+		s1[n] = s2[n];
+	return (dest);
 }
